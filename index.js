c8e1d3c79989e8417554b7c79367d6080d16034d
adelphes <adelphes@gmail.com>
2020-06-30 15:32:52 +0100

    code tidy

--- index.js
+++ index.js
@@ -19,17 +19,3 @@ exports.Parameter = require('./lib/Parameter').Parameter;
 exports.TypeVariable = require('./lib/TypeVariable').TypeVariable;
 exports.TypeArgument = require('./lib/TypeVariable').TypeArgument;
 exports.InferredTypeArgument = require('./lib/TypeVariable').InferredTypeArgument;
-
-// const { loadAndroidLibrary } = require('./android-library');
-// const { CEIJavaType } = require('./lib/JavaType');
-
-// const loadstart = Date.now();
-// loadAndroidLibrary('android-25').then(types => {
-//     // @ts-ignore
-//     const packages = new Set([...types.values()].filter(t => t instanceof CEIJavaType).map(t => t.packageName));
-//     console.log(`Loaded ${types.size} types from ${packages.size} packages in ${Date.now() - loadstart}ms`);
-//     // const strarr = exports.signatureToType('java/util/ArrayList<Ljava/lang/String;>', types);
-//     // const strarr2 = exports.signatureToType('java/util/ArrayList<Ljava/lang/String;>', types);
-//     // const strarr3 = exports.signatureToType('jova/util/ArrayList', types);
-//     types.size;
-// })
